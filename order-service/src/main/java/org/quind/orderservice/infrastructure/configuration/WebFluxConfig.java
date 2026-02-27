3e226fa923b105a127b53015517c2b260d06a65a
Ca2020 <Carlossalcedo@gmail.com>
2026-02-26 22:02:06 -0500

    chore: limpieza de código y configuraciones de infraestructura

---
+++ order-service/src/main/java/org/quind/orderservice/infrastructure/configuration/WebFluxConfig.java
@@ -0,0 +1,9 @@
+package org.quind.orderservice.infrastructure.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+    // Configuración para Actuator y otros componentes reactivos si es necesario
+}
