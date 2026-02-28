4d0dbb826d9b2bd3dc14792fabe2cd5e8758ef96
Ca2020 <Carlossalcedo@gmail.com>
2026-02-27 19:43:35 -0500

    fix: resolver conflictos de puertos de db local e incluir dependencias correctas para swagger en spring boot 3.4

--- order-service/src/main/java/org/quind/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/org/quind/orderservice/OrderServiceApplication.java
@@ -3,7 +3,14 @@ package org.quind.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
 @SpringBootApplication
+@EnableR2dbcRepositories(basePackages = "org.quind.orderservice.infrastructure.adapter.out.persistence", includeFilters = @org.springframework.context.annotation.ComponentScan.Filter(type = org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE, classes = {
+        org.quind.orderservice.infrastructure.adapter.out.persistence.R2dbcOrderRepository.class,
+        org.quind.orderservice.infrastructure.adapter.out.persistence.R2dbcOrderItemRepository.class }))
+@EnableReactiveMongoRepositories(basePackages = "org.quind.orderservice.infrastructure.adapter.out.persistence", includeFilters = @org.springframework.context.annotation.ComponentScan.Filter(type = org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE, classes = org.quind.orderservice.infrastructure.adapter.out.persistence.MongoOrderEventRepository.class))
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
